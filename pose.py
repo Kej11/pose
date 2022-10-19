50c8dd18d9ded9187bce9cbb8822e3dbb746a26b
Kej11 <110511824+Kej11@users.noreply.github.com>
2022-10-18 22:03:21 -0400

    write csv

--- pose.py
+++ pose.py
@@ -60,7 +60,17 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidenc
 cap.release()
 cv2.destroyAllWindows()
 
+#import libraries
 import csv
 import os
 import numpy as np
 
+num_coords = len(results.post_landmarks.landmark) + len(results.face_landmarks.landmark)
+
+landmarks = ['class']
+for val in range(1,num_coords+1):
+    landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+
+with open('coords.csv', mode='w', newline='') as f:
+    csv_writer = csv.writier(f,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(landmarks)

9f6c5e87f85e4927a821b649394a44d92afc2756
Kej11 <110511824+Kej11@users.noreply.github.com>
2022-10-18 21:55:17 -0400

    added coded

--- pose.py
+++ pose.py
@@ -21,25 +21,35 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidenc
 
         #Recolor feed
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
         #Make detections
         results = holistic.process(image)
         
         #face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
         #recolor image back to BGR for rendering
+        image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
         #draw face landmarks
-        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+        mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                                 mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                                 )
 
         #right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                 )
 
         #left hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                 )
 
         #pose detection
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                 )
 
         #lables frame
         cv2.imshow('Raw Webcame Feed', image)
@@ -49,4 +59,8 @@ with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidenc
 
 cap.release()
 cv2.destroyAllWindows()
+
+import csv
+import os
+import numpy as np
 
